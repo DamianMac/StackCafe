5942e69e79a52cc6e1d56a7480288558c17cd96b
Andrew Harcourt <andrewh@uglybugger.org>
2017-10-30 22:44:26 +1000

    Waiter now correlates order payment and completion before handing coffee to customer

---
+++ src/StackMechanics.Waiter/AutofacModules/ServicesModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using StackMechanics.Waiter.Services;
+
+namespace StackMechanics.Waiter.AutofacModules
+{
+    public class ServicesModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<OrderDeliveryService>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+}
---
+++ src/StackMechanics.Waiter/Rules/WhenAnOrderIsPaidFor/CheckWhetherItHasAlreadyBeenMade.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Nimbus.Handlers;
+using StackMechanics.MessageContracts.Events;
+using StackMechanics.Waiter.Services;
+
+namespace StackMechanics.Waiter.Rules.WhenAnOrderIsPaidFor
+{
+    public class CheckWhetherItHasAlreadyBeenMade : IHandleCompetingEvent<OrderPaidForEvent>
+    {
+        private readonly IOrderDeliveryService _orderDeliveryService;
+
+        public CheckWhetherItHasAlreadyBeenMade(IOrderDeliveryService orderDeliveryService)
+        {
+            _orderDeliveryService = orderDeliveryService;
+        }
+
+        public async Task Handle(OrderPaidForEvent busEvent)
+        {
+            _orderDeliveryService.MarkAsPaid(busEvent.OrderId);
+        }
+    }
+}
