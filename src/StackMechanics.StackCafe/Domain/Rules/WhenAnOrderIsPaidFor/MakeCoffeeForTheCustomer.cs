c977a28ec7a9e891a803e578c8901c46e2092abc
Ben Tefay <btefay@gmail.com>
2017-11-06 12:17:34 +1000

    Green

--- src/StackMechanics.StackCafe/Domain/Rules/WhenAnOrderIsPaidFor/MakeCoffeeForTheCustomer.cs
+++ src/StackMechanics.StackCafe/Domain/Rules/WhenAnOrderIsPaidFor/MakeCoffeeForTheCustomer.cs
@@ -16,7 +16,7 @@ namespace StackMechanics.StackCafe.Domain.Rules.WhenAnOrderIsPaidFor
 
         public void Handle(OrderPaidForEvent e)
         {
-            throw new NotImplementedException();
+            _barista.MakeOrder(e.Order);
         }
     }
 }
