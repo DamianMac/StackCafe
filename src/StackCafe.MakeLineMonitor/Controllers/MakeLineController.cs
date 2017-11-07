62eda992fa3f692d3072650842d084002f696ff0
Ben Tefay <btefay@gmail.com>
2017-11-07 12:25:55 +1000

    Line monitor

--- src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
+++ src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Web.Mvc;
 using StackCafe.MakeLineMonitor.Models;
+using StackCafe.MakeLineMonitor.Services;
 
 namespace StackCafe.MakeLineMonitor.Controllers
 {
     public class MakeLineController : Controller
     {
+        private readonly IMakeLineService _service;
+
+        public MakeLineController(IMakeLineService service)
+        {
+            _service = service;
+        }
+
         public ActionResult Index()
         {
-            var model = new MakeLineViewModel("Doppio", "Flat white", "Long black", "Doppio", "Doppio");
+            var model = new MakeLineViewModel(_service.Get());
             return View(model);
         }
     }
--- src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
+++ src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StackCafe.MakeLineMonitor.Services
 {
     public class MakeLineService : IMakeLineService
     {
+        private readonly Dictionary<Guid, string> _items = new Dictionary<Guid,string>();
+
         public void Add(Guid orderId, string coffeeType)
         {
+            _items.Add(orderId, coffeeType);
         }
 
         public void Remove(Guid orderId)
         {
+            _items.Remove(orderId);
+        }
+
+        public string[] Get()
+        {
+            return _items.Values.ToArray();
         }
     }
 }
