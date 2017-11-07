6c7e084fb5ac4a5cbaa5cd38e0761eb8f6d735a9
Andrew Wickens <andrew.wickens@my.jcu.edu.au>
2017-11-07 13:06:54 +1000

    Implement makeline service logic, and hook it up to the makeline controller

--- src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
+++ src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Web.Mvc;
 using StackCafe.MakeLineMonitor.Models;
+using StackCafe.MakeLineMonitor.Services;
 
 namespace StackCafe.MakeLineMonitor.Controllers
 {
     public class MakeLineController : Controller
     {
+        private readonly IMakeLineService _makeLineService;
+        
+        public MakeLineController(IMakeLineService makeLineService)
+        {
+            _makeLineService = makeLineService;
+        }
+
         public ActionResult Index()
         {
-            var model = new MakeLineViewModel("Doppio", "Flat white", "Long black", "Doppio", "Doppio");
+            var model = new MakeLineViewModel(_makeLineService.GetCoffeesOnMakeLine());
             return View(model);
         }
     }
--- src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
+++ src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StackCafe.MakeLineMonitor.Services
 {
     public class MakeLineService : IMakeLineService
     {
+        private readonly Dictionary<Guid, string> _ordersOnMakeLine;
+
+        public MakeLineService()
+        {
+            _ordersOnMakeLine = new Dictionary<Guid, string>();
+        }
+
         public void Add(Guid orderId, string coffeeType)
         {
+            _ordersOnMakeLine.Add(orderId, coffeeType);
         }
 
         public void Remove(Guid orderId)
         {
+            _ordersOnMakeLine.Remove(orderId);
+        }
+
+        public string[] GetCoffeesOnMakeLine()
+        {
+            return _ordersOnMakeLine.Select(s => s.Value).ToArray();
         }
     }
 }
