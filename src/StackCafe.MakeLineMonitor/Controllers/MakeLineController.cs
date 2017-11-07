ece11f9418b9266a7c03c8a11bd5d906bc107f96
Chris Briggs <chris@lup.com.au>
2017-11-07 12:21:47 +1000

    Fixed Make Line

--- src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
+++ src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Web.Mvc;
 using StackCafe.MakeLineMonitor.Models;
+using StackCafe.MakeLineMonitor.Services;
 
 namespace StackCafe.MakeLineMonitor.Controllers
 {
     public class MakeLineController : Controller
     {
+        public MakeLineController(IMakeLineService makeLineService)
+        {
+            _MakeLineService = makeLineService;
+        }
+
+        private IMakeLineService _MakeLineService { get; }
+
         public ActionResult Index()
         {
-            var model = new MakeLineViewModel("Doppio", "Flat white", "Long black", "Doppio", "Doppio");
+            var coffees = this._MakeLineService.GetAll();
+            var model = new MakeLineViewModel(coffees);
             return View(model);
         }
     }
--- src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
+++ src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StackCafe.MakeLineMonitor.Services
 {
     public class MakeLineService : IMakeLineService
     {
+        private Dictionary<Guid, string> ordersDictionary;
+
+        public MakeLineService()
+        {
+            this.ordersDictionary = new Dictionary<Guid, string>();
+        }
+
         public void Add(Guid orderId, string coffeeType)
         {
+            this.ordersDictionary.Add(orderId, coffeeType);
         }
 
         public void Remove(Guid orderId)
         {
+            this.ordersDictionary.Remove(orderId);
+        }
+        
+        public string[] GetAll()
+        {
+            return this.ordersDictionary.Values.ToArray();
+        }
+
+        public bool IsEmpty()
+        {
+            return this.ordersDictionary.Values.Count == 0;
         }
     }
 }
