e47f65d365669217ccba8ffc83004ec51c88b71f
Yann Vincent <yannvincent@live.com>
2017-11-07 13:14:07 +1000

    Initial make line

--- src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
+++ src/StackCafe.MakeLineMonitor/Controllers/MakeLineController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Web.Mvc;
+using StackCafe.MakeLineMonitor.Models;
+using StackCafe.MakeLineMonitor.Services;
 
 namespace StackCafe.MakeLineMonitor.Controllers
 {
     public class MakeLineController : Controller
     {
+        private readonly IMakeLineService _makeLineService;
+
+        public MakeLineController(IMakeLineService makeLineService)
+        {
+            _makeLineService = makeLineService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var model = new MakeLineViewModel(this._makeLineService.CurrentOrders);
+            return View(model);
         }
     }
 }
--- src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
+++ src/StackCafe.MakeLineMonitor/Services/MakeLineService.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Nimbus;
+using Serilog;
 
 namespace StackCafe.MakeLineMonitor.Services
 {
     public class MakeLineService : IMakeLineService
     {
+        private readonly IDictionary<Guid, string> line = new Dictionary<Guid, string>();
+
         public void Add(Guid orderId, string coffeeType)
         {
-            throw new NotImplementedException();
+            Log.Information("Add Order {OrderId} Coffer {CoffeeType}", orderId, coffeeType);
+           this.line.Add(orderId, coffeeType);
         }
 
         public void Remove(Guid orderId)
         {
-            throw new NotImplementedException();
+            if (line.ContainsKey(orderId))
+            {
+                Log.Information("Remove order {OrderId}", orderId);
+                line.Remove(orderId);
+            }
         }
+
+        public string[] CurrentOrders => this.line.Values.ToArray();
     }
 }
