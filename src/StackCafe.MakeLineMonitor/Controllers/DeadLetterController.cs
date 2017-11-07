be1f6149d1148350fc6ae2d86f2c484f98a30239
Terry Rossow <terryrossow@hotmail.com>
2017-11-07 17:07:33 +1000

    Broken MVC

---
+++ src/StackCafe.MakeLineMonitor/Controllers/DeadLetterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Nimbus;
+using StackCafe.MakeLineMonitor.Models;
+
+namespace StackCafe.MakeLineMonitor.Controllers
+{
+    public class DeadLetterController : Controller
+    {
+        private readonly IBus _bus;
+
+        public DeadLetterController(IBus bus)
+        {
+            this._bus = bus;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var numberOfDeadLetters = await this._bus.DeadLetterOffice.Count();
+            if (numberOfDeadLetters < 1)
+            {
+                return View(new DeadLetterViewModel());
+            }
+
+            var nimbusMessage = await this._bus.DeadLetterOffice.Peek();
+            return View(new DeadLetterViewModel(numberOfDeadLetters, nimbusMessage));
+        }
+    }
+}
---
+++ src/StackCafe.MakeLineMonitor/Models/DeadLetterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Nimbus;
+
+namespace StackCafe.MakeLineMonitor.Models
+{
+    public class DeadLetterViewModel
+    {
+        private readonly NimbusMessage message;
+        private readonly int numberOfDeadLetters;
+        public DeadLetterViewModel(int numberOfDeadLetters, NimbusMessage message)
+        {
+            this.numberOfDeadLetters = numberOfDeadLetters;
+            this.message = message;
+        }
+
+        public DeadLetterViewModel() : this(0, null)
+        {
+        }
+
+        public string Title
+        {
+            get { return this.message == null ? "No Dead Letters!" : "Decide This Message's Fate - there are " + this.numberOfDeadLetters + " more to deal with:"; }
+        }
+
+        public string Message
+        {
+            get { return this.message == null ? string.Empty : JsonConvert.SerializeObject(this.message.Payload); }
+        }
+    }
+}
