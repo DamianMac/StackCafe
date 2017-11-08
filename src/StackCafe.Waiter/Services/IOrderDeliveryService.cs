b09341e34f7ea40eac85a7279fdfa25b3c53f48e
Andrew Wickens <andrew.wickens@my.jcu.edu.au>
2017-11-08 15:17:56 +1000

    Plumb through more waiter things

--- src/StackCafe.Waiter/Services/IOrderDeliveryService.cs
+++ src/StackCafe.Waiter/Services/IOrderDeliveryService.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StackCafe.Waiter.Services
 {
     public interface IOrderDeliveryService
     {
+        void AddUnmadeOrder(Guid orderId, List<string> items);
+        void MarkItemAsMade(Guid orderId, string itemCode);
         void MarkAsPaid(Guid orderId);
         void MarkAsMade(Guid orderId);
+        bool HasBeenMade(Guid orderId);
+        bool HasBeenPaid(Guid orderId);
     }
 }
