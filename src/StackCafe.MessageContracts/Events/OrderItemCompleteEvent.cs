0bde1c42251e31a092a063dbf34b4f311e6303a8
Andrew Wickens <andrew.wickens@my.jcu.edu.au>
2017-11-08 13:12:38 +1000

    Plumb through prepare food item command & order item complete event

---
+++ src/StackCafe.MessageContracts/Events/OrderItemCompleteEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Nimbus.MessageContracts;
+
+namespace StackCafe.MessageContracts.Events
+{
+    public class OrderItemCompleteEvent : IBusEvent
+    {
+        public Guid OrderId { get; set; }
+
+        public string ItemCode { get; set; }
+    }
+}
