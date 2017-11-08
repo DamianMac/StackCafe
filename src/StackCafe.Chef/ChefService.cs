44330f21ce9af2bfeed59f7cf49f34a86f203bb1
Andrew Wickens <andrew.wickens@my.jcu.edu.au>
2017-11-08 12:09:20 +1000

    Implement Chef service

---
+++ src/StackCafe.Chef/ChefService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Autofac;
+using Serilog;
+using StackCafe.Common.Configuration;
+using StackCafe.Common.Logging;
+
+namespace StackCafe.Chef
+{
+    public class ChefService
+    {
+        private IContainer _container;
+
+        public void Start()
+        {
+            Log.Logger = DefaultLoggerConfiguration.CreateLogger();
+
+            try
+            {
+                _container = IoC.LetThereBeIoC();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occurred");
+                Log.CloseAndFlush();
+                throw;
+            }
+        }
+
+        public void Stop()
+        {
+            _container?.Dispose();
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ src/StackCafe.Chef/ForceLoadStackMechanicsCommonLogging.cs
@@ -0,0 +1,8 @@
+﻿using StackCafe.Common.Logging;
+
+namespace StackCafe.Chef
+{
+    public class ForceLoadStackCafeCommonLogging : AssemblyMarker
+    {
+    }
+}
